d576b312e08fb5440f869778579be76c2afe2abc
dylanmcca <dylanmcca@gmail.com>
2025-12-09 20:54:15 +0000

    testing blog & adding views to app

--- trekkie_blog/urls.py
+++ trekkie_blog/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from trekkies.views import trekkie_blog
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', trekkie_blog, name='trekkie_blog'),
 ]
--- trekkies/views.py
+++ trekkies/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def trekkie_blog(request):
+    return HttpResponse("Hello, trekkies - welcome to the blog to discuss all things Star Trek!")
