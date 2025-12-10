e79bbdade39f2d11c2d4f81f6145d0ceab035a89
dylanmcca <dylanmcca@gmail.com>
2025-12-10 22:00:16 +0000

    building a Post modal

--- trekkie_blog/settings.py
+++ trekkie_blog/settings.py
@@ -98,6 +98,11 @@ else:
         }
     }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
