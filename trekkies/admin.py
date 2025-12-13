6c836d066123abb4fe90f59bacf3e1b4e1a362d9
dylanmcca <dylanmcca@gmail.com>
2025-12-13 13:16:37 +0000

    Fix admin configuration and models

--- trekkies/admin.py
+++ trekkies/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
-admin.register(Post)
-admin.site.register(Comment)
-
-
+@admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
