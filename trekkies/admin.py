d931e0e15c4fc6395e2844f2743c76eca4949205
dylanmcca <dylanmcca@gmail.com>
2025-12-13 13:05:18 +0000

    adding comments to django admin

--- trekkies/admin.py
+++ trekkies/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
-admin.site.register(Post)
+admin.register(Post)
+admin.site.register(Comment)
+
+
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
--- trekkies/models.py
+++ trekkies/models.py
@@ -19,14 +19,13 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
-    author = models.ForeignKey(User, on_delete=models.CASCADE,related_name="commenter")
-    email = models.EmailField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
 
-class Meta:
+    class Meta:
         ordering = ['created_on']
 
-def __str__(self):
-        return f"Comment {self.body} by {self.name}"
+    def __str__(self):
+        return f"Comment {self.body} by {self.author.username}"
