0f05e39564bb8f04f766e7b88c7b83ba6f6a557b
dylanmcca <dylanmcca@gmail.com>
2025-12-15 20:06:18 +0000

    adding additional summernote fields & capability

--- trekkies/admin.py
+++ trekkies/admin.py
@@ -4,6 +4,10 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
 
 @admin.register(Comment)
