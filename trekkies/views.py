9bd3b15f7255277b8a4b8c4f3db5e7265794f217
dylanmcca <dylanmcca@gmail.com>
2025-12-16 19:33:00 +0000

    adding image directory & adding image

--- trekkies/views.py
+++ trekkies/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
@@ -18,3 +18,26 @@ class PostDetail(generic.DetailView):
     template_name = 'trekkies/post_detail.html'
     slug_field = 'slug'
     context_object_name = 'post'
+
+def post_detail(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "blog/post_detail.html",
+        {"post": post},
+    )    
