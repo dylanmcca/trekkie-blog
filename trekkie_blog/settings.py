3d27b7d36a22564f14e64b54635354fd4a82d350
dylanmcca <dylanmcca@gmail.com>
2025-12-18 09:56:45 +0000

    added AllAuth & styling from base.html to Login Logout & Register pages

--- trekkie_blog/settings.py
+++ trekkie_blog/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = "AVRhex3D=9.^ulD!zQ3LQ6qJ)@9ZP3gGk$e&+r+!zF^k$y8t#(1"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', 'localhost' ]
 
