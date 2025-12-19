a76f662528030a07412e9aa8d2f7f3549cca7f43
dylanmcca <dylanmcca@gmail.com>
2025-12-19 22:11:04 +0000

    Fix whitenoise storage for production

--- trekkie_blog/settings.py
+++ trekkie_blog/settings.py
@@ -166,7 +166,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Media files (uploaded images)
 MEDIA_URL = '/media/'
