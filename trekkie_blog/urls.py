08998d46ccb8ebf84c1830ac9a3ab809c602ff9b
dylanmcca <dylanmcca@gmail.com>
2025-12-19 22:16:21 +0000

    Serve media files in production

--- trekkie_blog/urls.py
+++ trekkie_blog/urls.py
@@ -28,6 +28,5 @@ urlpatterns = [
     path("", include("trekkies.urls"), name="trekkies-urls"),
 ]
 
-# Serve media files in development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve media files
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
