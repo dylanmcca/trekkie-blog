44928e81dbf30de06178c2c613bf8489f00ec964
dylanmcca <dylanmcca@gmail.com>
2025-12-30 21:26:11 +0000

    added bold styling to all authours of comments & a line below each comment in post_detail.html for easier reading & identifying individual comments

--- trekkie_blog/settings.py
+++ trekkie_blog/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get(
     )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', 'localhost']
