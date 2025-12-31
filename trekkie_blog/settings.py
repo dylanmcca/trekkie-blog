518a2b9f3cb2ece573b3946210b4c3fd95469498
dylanmcca <dylanmcca@gmail.com>
2025-12-31 10:35:01 +0000

    Moved all images for ReadMe into a separate folder & relinked to each to clean up root directory

--- trekkie_blog/settings.py
+++ trekkie_blog/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get(
     )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', 'localhost']
